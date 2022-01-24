244c053b7927883ba41535d504ff90abde91e5f3
ThiliniErandi <thilini.e97@gmail.com>
2022-01-24 19:23:22 +0530

    welcome page updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Welcome from './Welcome';
+import Welcome from '../src/pages/Welcome';
 
 function App() {
   return (
---
+++ src/components/SocialMediaIcons.js
@@ -0,0 +1,30 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitterSquare  } from "@fortawesome/free-brands-svg-icons";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+export default function SocialMediaIcons() {
+  return <div className="SocialMediaIcons" 
+              style={
+                    {  color: '#fff',
+                        fontSize: '70px',
+                        paddingRight: '20px',
+                        paddingLeft: '1240px',
+                        marginTop: '20px',
+                        position: 'absolute',
+                        float: 'right' }
+            }>
+            <FontAwesomeIcon 
+                className='social-icon'
+                style={{ padding: '10px' }}
+                icon={ faTwitterSquare } />
+            <FontAwesomeIcon 
+                className='social-icon'
+                style={{ padding: '10px' }}
+                icon={ faFacebookSquare } />
+            <FontAwesomeIcon 
+                className='social-icon'
+                style={{ padding: '10px' }}
+                icon={ faLinkedin } />
+        </div>;
+}
