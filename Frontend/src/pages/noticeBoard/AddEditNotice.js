93f61bda9b668efec7c40d8dd639a03600a9acf5
Thilini Kumarawadu <thilini.e97@gmail.com>
2022-02-14 11:33:26 +0530

    some changes

---
+++ Frontend/src/pages/noticeBoard/AddEditNotice.js
@@ -0,0 +1,187 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { MDBBtn, MDBInput, MDBValidation } from 'mdb-react-ui-kit';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import Navbar from '../../components/Navbar';
+
+const initialState = {
+    title: '',
+    description: '',
+    // checkbox: '',
+    // imageUrl: ''
+}
+
+// const stuBatches = [
+//     { key:'1', value: 'First'},
+//     { key:'2', value: 'Second'},
+//     { key:'3', value: 'Third'},
+//     { key:'4', value: 'Fourth'}
+// ]
+
+const AddEditNotice = () => {
+
+    const [formValue, setFormValue] = useState(initialState);
+    // const [ checkboxErrMsg, setcheckboxErrorMsg ] = useState(null);
+    const { title, description } = formValue;
+
+    const getDate = () => {
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, "0");
+        let mm =  String(today.getMonth()).padStart(2, "0");
+        let yyyy = today.getFullYear();
+
+        today = dd + "/" + mm + "/" + yyyy;
+        return today; 
+    };  
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if(title && description ) {
+            const currentDate = getDate();
+            const updatedNoticeData = { ...formValue, date: currentDate };
+            const response = await axios.post("http://localhost:8070/notices/add", updatedNoticeData );
+            if(response.status === 201 ) {
+                toast.success("Notice added successfully");
+                setFormValue({ title: "", description: "" });
+                window.location = '/notices';
+            }else{
+                toast.error("Something went wrong");
+            }
+        }
+    };
+
+    const onInputChange = (e) => {
+        let { name, value } = e.target;
+        setFormValue({ ...formValue, [name]: value });
+    };
+
+    // const onUploadImage = (file) => {
+    //     console.log("file", file);
+    //     const formData = new FormData();
+    //     formData.append("file", file);
+    //     formData.append("upload_preset", "")
+    //     axios
+    //         .post("http://localhost:8070/notices/image/upload", formData)
+    //         .then((res) => {
+    //            toast.info("Image uploaded successfully");
+    //            setFormValue({ ...formValue }); 
+    //     })
+    //         .catch((err)=> {
+    //             toast.err("SOmething went wrong");
+    //         });
+        
+    // };
+
+    // const {id} = useParams();
+
+    // useEffect(() => {
+
+    // })
+
+    //   
+
+
+    return ( 
+        <><Navbar /><MDBValidation className='row '
+            style={{ margin: '50px', marginLeft: '400px', width:'800px'}}
+            noValidate onSubmit={handleSubmit}
+        >
+            <h3 style={{ textAlign: 'center', paddingBottom: '10px', fontFamily: 'Poppins' }}> Add Notice </h3>
+
+            <div style={{ margin: 'auto', padding: "15px", maxWidth: "400px", alignContent: "center" }}>
+
+                {
+                    /* <MDBInput
+                                        label='Notice_Id'
+                                        id="Notice_id"
+                                        name="id"
+                                        type="text"
+                                        style={{ display:'block', paddingRight:'333px', marginBottom:'20px', paddingBottom:'8px' }}
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.id}
+                                        disabled
+                                    /> */
+                }
+
+                <MDBInput 
+                    value={title || ""}
+                    name="title"
+                    label='Title'
+                    type="text"
+                    onChange={onInputChange}
+                    required validation='Please provide a title'
+                    invalid />
+
+                <br />
+
+                <MDBInput 
+                    value={description || ""}
+                    name="description"
+                    label='Description'
+                    type="text"
+                    textarea rows={6}
+                    onChange={onInputChange}
+                    required validation='Please provide a description'
+                    invalid />
+
+                <br />
+
+                {/* <MDBInput 
+                    type='file'
+                    onChange={(e) => onUploadImage(e.target.files)}
+                    required validation='Please provide a description'
+                    invalid />
+
+                <br /> */}
+
+                {/* <div style={{display: 'inline'}}>
+
+                    <p style={{display: 'inline', marginRight:'18px'}}>Student Batch/Baches:</p>
+
+                    <input type="checkbox"
+                        name="stu_batch"
+                        value="First"
+                        style={{display: 'inline', marginRight:'5px'}}
+                        onclick="return ValidatePetSelection();" /> 1 
+
+                    <input type="checkbox"
+                        name="stu_batch"
+                        value="Second"
+                        style={{display: 'inline', marginRight:'5px', marginLeft: '30px'}}
+                        onclick="return ValidatePetSelection();" /> 2 
+
+                    <input type="checkbox"
+                        name="stu_batch"
+                        value="Third"
+                        style={{display: 'inline', marginRight:'5px', marginLeft: '30px'}}
+                        onclick="return ValidatePetSelection();" /> 3 
+
+                    <input type="checkbox"
+                        name="stu_batch"
+                        value="Fourth"
+                        style={{display: 'inline', marginRight:'5px', marginLeft: '30px'}}
+                        onclick="return ValidatePetSelection();" /> 4
+                </div> */}
+               
+
+                <br />
+                <br />
+
+                <MDBBtn className='formBtn'
+                    type='submit'
+                    style={{ marginLeft: '50px', paddingInline: '40px', fontSize: '15PX', marginTop: '30px', marginRight:'20px'}}
+                >ADD
+                </MDBBtn>
+                <MDBBtn color='dark'
+                    style={{ paddingInline: '40px', fontSize: '15PX', marginTop: '20px' }}
+                    onclick={() => window.location("/notices")}
+                    >Go Back
+                </MDBBtn>
+            </div>
+        </MDBValidation></>
+    )
+}
+
+export default AddEditNotice;
--- Frontend/src/pages/noticeBoard/NoticeHome.js
+++ Frontend/src/pages/noticeBoard/NoticeHome.js
@@ -1,62 +1,79 @@
-// import React, { useState, useEffect } from 'react';
-// import { toast } from 'react-toastify';
-// // import axios from 'axios';
-// import { MDBRow, MDBCol, MDBContainer, MDBTypography } from 'mdb-react-ui-kit';
-// import Notices from './Notices';
+import React, { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { MDBRow, MDBCol, MDBContainer, MDBTypography } from 'mdb-react-ui-kit';
+import Notices from './Notices';
+import Navbar from '../../components/Navbar'
 
-// const NoticeHome = () => {
-//     const [ data, setData ] = useState([]);
+const NoticeHome = () => {
+    const [data, setData] = useState([]);
 
-//     useEffect(() => {
-//         loadNoticesData();
-//     }, [])
+    useEffect(() => {
+        loadNoticesData();
+    }, [])
 
-//     const loadNoticesData = async () => {
-//         // const response = await axios.get("");
-//         // if(response.status === 200) {
-//         //     setData(response.data)
-//         // } else {
-//         //     toast.error("Something went wrong");
-//         // }
-//     };
+    const loadNoticesData = async() => {
+        const response = await axios.get("http://localhost:8070/notices/view");
+        if (response.status === 200) {
+            setData(response.data)
+        } else {
+            toast.error("Something went wrong");
+        }
+    };
 
-//     console.log('data', data);
+    console.log('data', data);
 
-//     const handleDelete = () => {};
+    const handleDelete = async(id) => {
+        if (window.confirm("Are you sure that you wanted to delete this notice?")) {
+            const response = await axios.delete(`http://localhost:8070/notices/delete/$${id}`);
+            if (response.status === 200) {
+                toast.success("Notice deleted successfully");
+                loadNoticesData();
+            } else {
+                toast.error("Something went wrong");
+            }
+        }
+    };
 
-//     const excerpt = (str) => {
-//         if(str.length > 50){
-//             str = str.substring(0, 50) + " ... ";
-//         }
-//         return str;
-//     };
 
-//     return ( 
-//         <>
-//             <MDBRow>
-//                 {data.length === 0 && (
-//                     <MDBTypography className='text-center mb-0' tag='h2'>
-//                         No Notice Found
-//                     </MDBTypography>
-//                 )}
-//                 <MDBCol>
-//                     <MDBContainer>
-//                         <MDBRow>
-//                             { data && data.map(( item, index ) => (
-//                                 <Notices
-//                                     key={index}
-//                                     { ...item }
-//                                     excerpt={excerpt}
-//                                     handleDelete={handleDelete}
-//                                 />
-//                             ))}
-//                         </MDBRow>
-//                     </MDBContainer>
-//                 </MDBCol>
-//             </MDBRow>
-//         </>
+    const excerpt = (str) => {
+        if (str.length > 50) {
+            str = str.substring(0, 50) + " ... ";
+        }
+        return str;
+    };
 
-//     );
-// }; 
- 
-// export default NoticeHome;
+    return ( < >
+        <
+        Navbar / >
+        <
+        MDBRow > {
+            data.length === 0 && ( <
+                MDBTypography className = 'text-center mb-0'
+                tag = 'h2' >
+                No Notice Found <
+                /MDBTypography>
+            )
+        } <
+        MDBCol >
+        <
+        MDBContainer >
+        <
+        MDBRow > {
+            data && data.map((item, index) => ( <
+                Notices key = { index } {...item }
+                excerpt = { excerpt }
+                handleDelete = { handleDelete }
+                />
+            ))
+        } <
+        /MDBRow> < /
+        MDBContainer > <
+        /MDBCol> < /
+        MDBRow > <
+        />
+
+    );
+};
+
+export default NoticeHome;
